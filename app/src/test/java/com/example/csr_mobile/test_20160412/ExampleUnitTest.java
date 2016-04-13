72a7176ad363be1c45a80c99038aa991b8571b23
CSR_Mobile <CSR_Mobile@CSRMOB.local>
2016-04-13 13:34:04 +0900

    Initial commit

---
+++ app/src/test/java/com/example/csr_mobile/test_20160412/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.csr_mobile.test_20160412;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
